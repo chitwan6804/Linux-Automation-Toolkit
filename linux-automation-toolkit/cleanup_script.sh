144e0d4781832e1278c1f7de8965257981b40425
Chitwan <128251060+chitwan6804@users.noreply.github.com>
2025-07-31 01:44:33 +0530

    Delete linux-automation-toolkit directory

--- linux-automation-toolkit/cleanup_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TARGET_DIR="$HOME/linux-automation-toolkit/logs"
-mkdir -p "$TARGET_DIR"
-
-find "$TARGET_DIR" -name "*.log" -type f -mtime +7 -exec rm {} \;
-echo "Old .log files removed from $TARGET_DIR"
